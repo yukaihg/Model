bbe5d79f8ae58d9814361d8def8680aceaea7ba5
yukaihg <yukaihg@gmail.com>
2012-07-02 17:30:34 -0700

    added delete

--- Question.js
+++ Question.js
@@ -1,30 +1,23 @@
 var es = require('com.izaakschroeder.elasticsearch'),
-	db = es.connect('localhost');
-	index = db.index('big-data/magic-cards');	//we will use presenter/questions
+	db = es.connect('localhost'),
+	index = db.index('big-data'),	//we will use presenter
+	mapping = index.mapping('songs');	//use questions
 
 var Question = function(){
 }
 
 //create a new question
-Question.prototype.put = function(JSON, callback){
+Question.prototype.put = function(data, callback){
 
+	var document = mapping.document(data._id);
+	document.set(data._source, function(){});
 }
 
 //search based on uid
-Question.prototype.get = function(uid, callback){
-
-	//need a query builder
-	var data = {
-		query: {
-			match_all: { }
-		},
-		from: 0,
-		size: 20,
-		sort: [  ],
-		facets: { }
-	};
-
-	index.search(data, function(err, data){console.log(data)});
+Question.prototype.get = function(data, callback){
+	mapping.search(data, function(err, data){
+		console.log(data.hits.total);
+	});
 }
 
 //update a uid
@@ -33,7 +26,11 @@ Question.prototype.post = function(uid, callback){
 }
 
 //delete a uid
-Question.portotype.delete = function(uid, callback){
+Question.prototype.delete = function(uid, callback){
+	var document = mapping.document(uid);
+	document.delete(function(){
+		console.log('removed');
+	})
 
 }
 
--- test.js
+++ test.js
@@ -1,3 +1,32 @@
+//TODO: QUERY BUILDER
+
 var question = require('./Question.js');
 
-question.post('d', function(){});
+//*****************SEARCH***********************
+//some search query for example
+var searchData = {
+	query: {
+		match_all: { }
+	},
+	from: 0,
+	size: 20,
+	sort: [  ],
+	facets: { }
+};
+
+question.get(searchData, function(){});
+
+//*****************INSERT***********************
+var insertData = {
+	'_id':'someID',
+	'_source': {'someKey':'someValue'}
+};
+
+//inserting into es
+question.put(insertData, function(){});
+
+
+//*****************DELETE***********************
+
+//deleting this uid
+question.delete(insertData._id);
